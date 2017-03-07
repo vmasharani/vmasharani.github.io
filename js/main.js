b993099da5f53c2520e59ba58d5bf07690317787
vmasharani <vmasharani@mica.edu>
2017-03-06 23:04:07 -0500

    created all project pages, filled in content for first one (reflexive)

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
       if (event.target.id == projects[i].id){
           modal.style.display = "block";
           cover.style.display="block";
-          $('#projectpage').load(event.target.id + '.html');
+          $('#projectcontainer').load(event.target.id + '.html');
       } else {
         console.log("dumb");
       }
@@ -34,7 +34,7 @@ $(document).ready(function(){
       if (event.target == cover) {
           modal.style.display = "none";
           cover.style.display="none";
-          $('#projectpage').empty();
+          $('#projectcontainer').empty();
 
 
       }

b02ce15f7841569e9d9345e5eae02a44f747ba1b
vmasharani <vmasharani@mica.edu>
2017-04-24 01:08:40 -0400

    testing servers

--- imageget/js/main.js
+++ imageget/js/main.js
@@ -1,38 +1,38 @@
 $(function() {
-      // var params = {
-      //       q:"Maryland Institute College of Art"
-      //             };
+//       var params = {
+//             q:"Maryland Institute College of Art"
+//                   };
+//
+//       var data=   $.ajax({
+//             url: "https://api.cognitive.microsoft.com/bing/v5.0/images/trending" ,
+//             beforeSend: function(xhrObj){
+//                 // Request headers
+//                 xhrObj.setRequestHeader("Content-Type","multipart/form-data");
+//                 xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","5b267c9324ae4318bd66e2cc297753c9");
+//             },
+//             type: "GET",
+//             dataType: 'json',
+//
+//             // Request body
+//             data: "{body}",
+//         })
+//
+//         .done(function(data) {
+//             alert("success");
+//         })
+//
+//         .fail(function() {
+//             alert("error");
+//         });
+//         // console.log(data);
+//         setTimeout(function(){
+//           localStorage.setItem('a23trend',JSON.stringify(data));
+//         },1000);
+//         // console.log(JSON.stringify(data));
       //
-      // var data=   $.ajax({
-      //       url: "https://api.cognitive.microsoft.com/bing/v5.0/images/trending" ,
-      //       beforeSend: function(xhrObj){
-      //           // Request headers
-      //           xhrObj.setRequestHeader("Content-Type","multipart/form-data");
-      //           xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","5b267c9324ae4318bd66e2cc297753c9");
-      //       },
-      //       type: "GET",
-      //       dataType: 'json',
-      //
-      //       // Request body
-      //       data: "{body}",
-      //   })
-      //
-      //   .done(function(data) {
-      //       alert("success");
-      //   })
-      //
-      //   .fail(function() {
-      //       alert("error");
-      //   });
-      //   // console.log(data);
-      //   setTimeout(function(){
-      //     localStorage.setItem('a23trend',JSON.stringify(data));
-      //   },1000);
-      //   // console.log(JSON.stringify(data));
-        // console.log(a23string);
-
       var retrieve=localStorage.getItem('a23trend');
       var parsedretrieve=JSON.parse(retrieve);
+      console.log(parsedretrieve);
       // console.log(parsedretrieve.responseJSON.categories[0].tiles[0].image.contentUrl)
        $("#test").append("<img src='"+parsedretrieve.responseJSON.categories[0].tiles[0].image.contentUrl+"'/>'");
 
